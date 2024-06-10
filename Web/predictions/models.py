1bd8b1073fe19a446d71eb37b0a0a5ad37ee0156
DESKTOP-ONE4R81\ITSC <sangil9959@gmail.com>
2024-06-10 11:36:36 +0900

    predictions 앱 생성 및 모델 구성

---
+++ Web/predictions/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Predictions(models.Model):
+    date = models.DateField("예측일", auto_now_add=False)
+    time = models.TimeField("예측시간", auto_now_add=False)
+    loc = models.CharField("지점번호", max_length = 5) # 서울 : 108, 부산 : 159, 대전 : 133, 광주 : 156, 강릉 : 105
+    pm10 = models.FloatField("PM-10")
+    pm25 = models.FloatField("PM-2.5")
+    temperature = models.IntegerField("기온")
+    percipitation = models.FloatField("강수량")
